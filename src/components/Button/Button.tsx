b065789aac8de2957eb33de7e13900f5279b9ff3
unknown <Anthony>
2024-03-27 14:48:51 +0800

    feat : zongha dependdencies

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props ={
+    text: string
+}
+
+const Button : React.FC<Props> = ({text}) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
